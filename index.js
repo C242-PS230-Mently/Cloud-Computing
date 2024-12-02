5f74f0c39c179b62033af0aa42e246f62b9e77f6
Zpice <ihsanxcaddy009@gmail.com>
2024-12-03 01:19:53 +0800

    test cloud build

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
-app.listen(PORT,() => {
+app.listen(PORT,'0.0.0.0',() => {
     console.log(`Server is running on PORT ${PORT}`)
 });
