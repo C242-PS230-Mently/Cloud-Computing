40d8fa2a8e113bd3895965eeadd9d36bb68ddecd
unknown <ihsanxcaddy009@gmail.com>
2024-12-05 18:46:20 +0800

    update profile

--- controller/auth/secret.js
+++ controller/auth/secret.js
@@ -1,13 +1,15 @@
 import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 
 
-const client = new SecretManagerServiceClient();
-
-export async function getServiceAccountKey() {
-  const [version] = await client.accessSecretVersion({
-    name: `projects/${process.env.GCLOUD_PROJECT}/secrets/GOOGLE_APPLICATION_CREDENTIALS/versions/latest`,
-  });
-
-  const payload = version.payload.data.toString('utf8');
-  return JSON.parse(payload); 
-}
+async function accessSecretVersion(projectId, secretId, versionId) {
+    const client = new SecretManagerServiceClient();
+  
+    // Build the resource name of the secret version.
+    const name = `projects/${process.env.GCLOUD_PROJECT}/secrets/${process.env.GOOGLE_APPLICATION_CREDENTIALS}/versions/latest`;
+  
+    // Access the secret version.
+    const [response] = await client.accessSecretVersion({ name: name });
+  
+    // Return the secret data.
+    return response.payload.data.toString();
+  }
