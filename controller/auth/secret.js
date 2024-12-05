a50d63b261bc3ac33624fcc951dfed86da42f30e
unknown <ihsanxcaddy009@gmail.com>
2024-12-05 18:51:21 +0800

    add secret

--- controller/auth/secret.js
+++ controller/auth/secret.js
@@ -1,11 +1,12 @@
 import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 
-
-async function accessSecretVersion(projectId, secretId, versionId) {
+const projectName = process.env.GCLOUD_PROJECT;
+const credentials = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+async function accessSecretVersion(projectName, credentials) {
     const client = new SecretManagerServiceClient();
   
     // Build the resource name of the secret version.
-    const name = `projects/${process.env.GCLOUD_PROJECT}/secrets/${process.env.GOOGLE_APPLICATION_CREDENTIALS}/versions/latest`;
+    const name = `projects/${projectName}/secrets/${credentials}/versions/latest`;
   
     // Access the secret version.
     const [response] = await client.accessSecretVersion({ name: name });
